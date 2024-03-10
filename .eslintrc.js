760468072e69ef1f90abec830323c676c7e60d1a
do-ald533 <doouglas.aldred533@gmail.com>
2024-03-10 22:05:05 +0000

    init

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+        "plugin:simple-import-sort/recommended"
+    ],
+    plugins: [
+        "@typescript-eslint",
+        "prettier",
+        "simple-import-sort"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "prettier/prettier": "error",
+        "simple-import-sort/imports": "error",
+        "simple-import-sort/exports": "error",
+        "no-console": "error",
+        "@typescript-eslint/no-unused-vars": "error",
+        "@typescript-eslint/consistent-type-definitions": ["error", "type"]
+    },
+    "env": {
+        "es2021": true,
+        "node": true
+    }
+}
